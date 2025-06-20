c0d7702ffe08b47d3597ea16c57f3f4652bb49ae
agustin <114094@tecnicatura.frc.utn.edu.ar>
2025-06-20 14:23:52 -0300

    Commit incial

---
+++ src/main/java/tesis/tesisenvios/TesisEnviosApplication.java
@@ -0,0 +1,13 @@
+package tesis.tesisenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisEnviosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisEnviosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tesis/tesisenvios/TesisEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package tesis.tesisenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisEnviosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
